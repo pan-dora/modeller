a2398ac5599ea4451919e11a042fdfc12ee10415
Christopher Johnson <christopher.johnson@wikimedia.de>
2016-08-16 13:38:25 +0200

    adds mustache

---
+++ modeller-fedora/src/main/java/org/blume/modeller/TestTemplate.java
@@ -0,0 +1,47 @@
+package org.blume.modeller;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+import org.blume.modeller.common.uri.FedoraPrefixes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestTemplate {
+
+    List<Item> items() {
+        return Arrays.asList(
+                new Item(Arrays.asList(
+                        new Prefix(FedoraPrefixes.RDFS),
+                        new Prefix(FedoraPrefixes.MODE)),
+                        "http://localhost:8888/iiif/")
+        );
+    }
+
+    static class Item {
+        Item(List<Prefix> prefixes, String serviceURI) {
+            this.prefixes = prefixes;
+            this.serviceURI = serviceURI;
+        }
+
+        List<Prefix> prefixes;
+        String serviceURI;
+    }
+
+    static class Prefix {
+        Prefix(String prefix) {
+            this.prefix = prefix;
+        }
+
+        String prefix;
+    }
+
+    public static void main(String[] args) throws IOException {
+        MustacheFactory mf = new DefaultMustacheFactory();
+        Mustache mustache = mf.compile("template/sparql-update.mustache");
+        mustache.execute(new PrintWriter(System.out), new TestTemplate()).flush();
+    }
+}
